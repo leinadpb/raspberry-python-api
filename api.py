f4b4f0eb2520d83bf8d337c3a5b2b15cbd0eaee4
Daniel Pena Berroa <digrape07@gmail.com>
2020-09-20 19:49:34 +0100

    changes

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from quart import Quart
 import RPi.GPIO as GPIO
+import subprocess
 
 GPIO.cleanup()
 
@@ -17,6 +18,11 @@ app.register_blueprint(led_api, url_prefix="/v1/led")
 app.register_blueprint(sound_api, url_prefix="/v1/sound")
 app.register_blueprint(vibration_api, url_prefix="/v1/vibration")
 
+def restart():
+    command = "/usr/bin/sudo /sbin/shutdown -r now"
+    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+    output = process.communicate()[0]
+    print output
 
 def CreateWifiConfig(SSID, password):
   config_lines = [
@@ -33,8 +39,8 @@ def CreateWifiConfig(SSID, password):
 
   with open("/etc/wpa_supplicant/wpa_supplicant.conf", "a+") as wifi:
     wifi.write(config)
-
   print("Wifi config added")
+  restart()
 
 
 # Test Endpoint
@@ -46,7 +52,7 @@ async def hello():
 @app.route("/v1/config/wifi/<ssid>/<password>")
 async def wifi(ssid, password):
     CreateWifiConfig(ssid, password)
-    return "WiFi configuration!" + str(ssid) + ":" + str(password)
+    return "WiFi configuration set: SSID: " + str(ssid) + ", PASSWORD:" + str(password)
 
 if (__name__ == "__main__"):
     app.run(debug=True, host="0.0.0.0", port=80)
